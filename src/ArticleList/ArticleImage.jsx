6e021726ea56ed11992422935245fde96f99d5cc
yiyiingit <jerryyiyi@126.com>
2019-10-30 09:51:45 -0700

    assignment8

--- src/ArticleList/ArticleImage.jsx
+++ src/ArticleList/ArticleImage.jsx
@@ -3,7 +3,9 @@ import styles from './ArticleImage.module.css'
 
 const ArticleImage = props =>{
 	return (
-            <img src = {props.url} className = {styles.img} />  	
+            <img src = {props.url} 
+            	alt = {props.title}
+            className = {styles.img} />  	
 	);
 
 };
--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -8,12 +8,13 @@ const ArticleListItem = props =>{
         <article >
             <div className ={styles.box}>
             <ArticleImage url = {props.article.image._url} title = {props.article.title} />
-            <div className = {styles.div}>
-            <h1 className = {styles.title}>{props.article.title}</h1>
-            <p className ={styles.p}> {props.article.shortText} </p>
-            <time className= {styles.time} dateTime={props.article.pubYear}> {props.article.pubDate} </time>
-            <SlugButton buttonText = {props.article.author} slug = {props.article.slug}/> 
-            </div>
+                <div className = {styles.div}>
+                    <h1 className = {styles.title}>{props.article.title} </h1>
+                    <p className ={styles.p}> {props.article.shortText} </p>
+                    <time className= {styles.time} dateTime={props.article.pubYear}> {props.article.pubDate} </time>
+                    <SlugButton buttonText = {props.article.author} slug = {props.article.slug}/> 
+                </div>
+                
             </div>
         </article> 
     );
